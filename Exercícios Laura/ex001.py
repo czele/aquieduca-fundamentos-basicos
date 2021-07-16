546cc9dd256f1daf37433cee93e0650d4807d95e
Camila <camila_zele@hotmail.com>
2021-07-15 23:14:46 -0300

    ex 3 feito

--- Exercícios Laura/ex001.py
+++ Exercícios Laura/ex001.py
@@ -1,2 +1,3 @@
 x=input('Digite um número: ')
+#Se der tempo adicionar um if
 print('O número informado foi {}'.format(x))
---
+++ Exercícios Laura/ex003.py
@@ -0,0 +1,3 @@
+c=float(input('Digite a medida centímetros: '))
+print(c*0.01)
+#Colocar um if se for int ou se for float
