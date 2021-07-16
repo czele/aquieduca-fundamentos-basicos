1a9c05dda2c717d71963a6fc1707f76d72a6c0ee
Camila <camila_zele@hotmail.com>
2021-07-15 23:05:39 -0300

    ex 2 feito

---
+++ Exercícios Laura/ex002.py
@@ -0,0 +1,8 @@
+import statistics
+
+n1=int(input('Digite a primeira nota: '))
+n2=int(input('Digite a segunda nota: '))
+n3=int(input('Digite a terceira nota: '))
+n4=int(input('Digite a quarta nota: '))
+
+print(statistics.mean([n1, n2, n3, n4]))
