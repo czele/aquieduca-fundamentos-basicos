72058623883a813f96582539478f1a0ecb77696e
Camila <camila_zele@hotmail.com>
2021-07-22 23:04:18 -0300

    ex harry (falta put)

---
+++ FlaskHarry/app.py
@@ -0,0 +1,25 @@
+from flask import Flask
+app = Flask(__name__)
+
+app.config['TESTING'] = True
+
+@app.route("/get", methods=['GET'])
+def get():
+    return 'Listado com sucesso!'
+
+@app.route("/post", methods=['POST'])
+def post():
+    return 'Postado com sucesso'
+
+@app.route("/put", methods=['PUT'])
+def put():
+    x=84/2
+    return(str(x))
+
+@app.route("/delete", methods=['DELETE'])
+def delete():
+    return('Este valor foi deletado')
+
+@app.route("/<id>", methods=['GET'])
+def getid(id):
+    return f'ID: {id}'
